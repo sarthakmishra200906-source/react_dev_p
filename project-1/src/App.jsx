304ebeaed06d7be87039afc33d42befebb5de059
sarthakmishra <sarthakmishra200906@gmail.com>
2026-09-13 13:46:58 +0530

    seted up basic header and foter by self

--- project-1/src/App.jsx
+++ project-1/src/App.jsx
@@ -1,13 +1,21 @@
-import React from 'react'
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './App.css'
+import React from 'react';
+import Header from './components/header';
+import Footer from './components/footer';
+import './App.css';
 
 export default function App() {
   return (
-    <div>
-      <h1>testing auto save </h1>
-      <p>lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.</p>
-    </div>
-  )
+    <>
+      <Header title="My App" />
+      <main className="container my-4">
+        <h2>Welcome to My App</h2>
+        <p>This is a simple React application using Vite, Bootstrap, and Tailwind CSS.</p>
+        <p>Pasted the global hosted link on TV browser to see the application.</p>
+        <p>checking re hosted website with working navbar from vs code manually</p>
+        <p>Testing the application on different devices.</p>
+      </main>
+      <Footer />
+    </>
+  );
 }
 
