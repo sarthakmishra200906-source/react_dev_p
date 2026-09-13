a7c726d316e4a7b23432a6b3d5bfcf2fe8022ae7
sarthakmishra <sarthakmishra200906@gmail.com>
2026-09-13 12:06:10 +0530

    making first project to practice learned concept

---
+++ project-1/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css'
+
+export default function App() {
+  return (
+    <div>
+      <h1>testing auto save </h1>
+      <p>lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.</p>
+    </div>
+  )
+}
+
